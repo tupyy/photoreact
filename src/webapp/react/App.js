1c17e1251170e808e16c3a284c3fba2c5533feab
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-07-23 13:28:54 +0200

    4 spaces

--- src/webapp/react/App.js
+++ src/webapp/react/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
