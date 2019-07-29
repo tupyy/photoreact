0b27e80b699ef5848739dfe6bca93145bc1596ea
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-07-29 19:47:30 +0200

    move read confi.env to dev environment
    in prod we use env variables

--- src/python/photogallery/settings/__init__.py
+++ src/python/photogallery/settings/__init__.py
@@ -13,12 +13,6 @@ from .utils import utils
 environ.setdefault('DJANGO_ENV', 'development')
 ENV = environ['DJANGO_ENV']
 
-config_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'config.env')
-if os.path.exists(config_file_path):
-    utils.load_env(config_file_path)
-else:
-    print('Config file not found')
-
 base_settings = [
     'components/logging.py',
     'components/common.py',
--- src/python/photogallery/settings/environments/development.py
+++ src/python/photogallery/settings/environments/development.py
@@ -6,7 +6,11 @@ This file contains all the settings that defines the development server.
 
 SECURITY WARNING: don't run with debug turned on in production!
 """
+import os
+
 from photogallery.settings.components.database import DATABASES_DEV
+from photogallery.settings.utils import utils
+
 DEBUG = True
 
 SECRET_KEY = "my_super_secret_key"
@@ -18,3 +22,11 @@ Database dev
 """
 DATABASES = DATABASES_DEV
 
+"""
+Load AWS variables
+"""
+config_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'config.env')
+if os.path.exists(config_file_path):
+    utils.load_env(config_file_path)
+else:
+    print('Config file not found')
