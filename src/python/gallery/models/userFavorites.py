fc01f3619620258833d1038882af8289bf0719ac
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-07-29 17:06:14 +0200

    add str method

--- src/python/gallery/models/userFavorites.py
+++ src/python/gallery/models/userFavorites.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from src.python.gallery.models import Album
+from gallery.models import Album
 
 
 class UserFavorites(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    album = models.ManyToManyField(Album, on_delete=models.CASCADE)
+    album = models.ManyToManyField(Album)
+
+    def __str__(self):
+        return self.user.__str__() + ' ' + self.album.__str__()
