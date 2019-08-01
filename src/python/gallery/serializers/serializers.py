52ff3ce6bb4209d28fcddfba2f60509b1ec5cf06
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-08-01 15:11:19 +0200

    add method fields for get/post method for photo and thumbnail

--- src/python/gallery/serializers/serializers.py
+++ src/python/gallery/serializers/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from gallery.models import Album, Photo, Category, Tag
+from gallery.utils.s3_manager import get_signed_url, put_signed_url
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -26,9 +27,23 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 
 class PhotoSerializer(serializers.ModelSerializer):
-    get_url = serializers.ReadOnlyField(source='get_signed_url')
-    put_url = serializers.ReadOnlyField(source='put_signed_url')
+    get_photo_url = serializers.SerializerMethodField(source='get_photo_signed_url')
+    get_thumbnail_url = serializers.SerializerMethodField(source='get_thumbnail_signed_url')
+    put_photo_url = serializers.SerializerMethodField(source='put_photo_signed_url')
+    put_thumbnail_url = serializers.SerializerMethodField(source='put_thumbnail_signed_url')
+
+    def get_photo_signed_url(self):
+        return get_signed_url(self.album.dirpath + '/' + self.filename)
+
+    def put_photo_signed_url(self):
+        return put_signed_url(self.album.dirpath + '/' + self.filename)
+
+    def get_thumbnail_signed_url(self):
+        return get_signed_url(self.thumbnail)
+
+    def put_thumbnail_signed_url(self):
+        return put_signed_url(self.thumbnail)
 
     class Meta:
         model = Photo
-        fields = ['__all__', 'get_url', 'put_url']
+        fields = ['__all__', 'get_photo_url', 'put_photo_url', 'get_thumbnail_url', 'put_thumbnail_url']
