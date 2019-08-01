b2eb51ee79993af4393158c13af7833c3499da16
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-01 19:38:12 +0200

    serialize only user and group id

--- src/python/gallery/serializers/auth_serializer.py
+++ src/python/gallery/serializers/auth_serializer.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 
@@ -6,6 +7,26 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ['password']
+
+
+class IDSerializerMixin:
+    def get_object(self, validated_data):
+        pk = int(validated_data)
+        if pk != validated_data:
+            raise ValueError('Value must be an int')
+        return get_object_or_404(self.Meta.model, pk=pk)
+
+
+class UserIDSerializer(IDSerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id']
+
+
+class GroupIDSerializer(IDSerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id']
 
 
 class GroupSerializer(serializers.ModelSerializer):
