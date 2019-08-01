34285fd5de581666e5fbc39e5f45673b497f33eb
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-08-01 16:14:06 +0200

    fix import name

--- src/python/gallery/models/__init__.py
+++ src/python/gallery/models/__init__.py
@@ -1,3 +1,3 @@
 from .album import Album, AccessPolicy, AlbumManager, AlbumAccessPolicy,Photo, PhotoAccessPolicy, PhotoManager
 from .category import Category, Tag
-from .userFavorites import UserFavorites
+from .user_favorites import UserFavorites
