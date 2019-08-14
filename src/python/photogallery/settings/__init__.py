9db128a9383e8acd84ca5874628ee213772423be
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-14 09:26:24 +0200

    change environment

--- src/python/photogallery/settings/__init__.py
+++ src/python/photogallery/settings/__init__.py
@@ -3,14 +3,14 @@ This is a django-split-settings main file.
 To change settings file:
 `DJANGO_ENV=production python manage.py runserver`
 """
-import os
 from os import environ
+
 from split_settings.tools import include
 
 from .utils import utils
 
 # Managing environment via DJANGO_ENV variable:
-environ.setdefault('DJANGO_ENV', 'dev_work')
+environ.setdefault('DJANGO_ENV', 'development')
 ENV = environ['DJANGO_ENV']
 
 base_settings = [
