96743c743539f1d74d41fdf79c0ef21a8286db65
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-14 09:23:16 +0200

    fix onwer permission

--- src/python/gallery/permissions/owner_permission.py
+++ src/python/gallery/permissions/owner_permission.py
@@ -9,13 +9,5 @@ class IsOwner(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         # Instance must have an attribute named `owner`.
-        if obj.owner == request.user:
-            return True
-
-        raise PermissionDenied({'reason': 'not owner'})
+        return obj.owner == request.user
