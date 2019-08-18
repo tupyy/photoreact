c1e28c69a4ecfcc1aa26ecf381a716e3dbd28a1e
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-18 20:10:26 +0200

    add filter class for activity log serializer

---
+++ src/python/gallery/filters/activity_log_filter.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters
+
+from gallery.models.album import ActivityLog
+
+
+class ActivityLogFilter(filters.FilterSet):
+    start_date = filters.DateFilter(field_name="date", lookup_expr='gte')
+    end_date = filters.DateFilter(field_name="date", lookup_expr='lte')
+
+    class Meta:
+        model = ActivityLog
+        fields = ['start_date', 'end_date']
