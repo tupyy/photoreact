454a7c14eb32775d62b541efaf6503edccd157f9
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-08-26 12:33:56 +0200

    class to wrap the login mechanism and to add token to credential

---
+++ back/tests/base_testcase.py
@@ -0,0 +1,24 @@
+import json
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+
+class BaseViewTestCase(TestCase):
+    token = None
+
+    def login(self, username, password):
+        client = APIClient()
+        response = client.post('/api/token/',
+                               data=json.dumps({"username": username,
+                                               "password": password}),
+                               content_type="application/json")
+        if response.status_code == 200:
+            self.token = response.data['access']
+            return True
+        return False
+
+    def get_client(self):
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        return client
