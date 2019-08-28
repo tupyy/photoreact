28e712f4f3cf81eb27b118762304c3b3b21bb905
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-28 09:08:50 +0200

    typo

--- back/src/gallery/views/__init__.py
+++ back/src/gallery/views/__init__.py
@@ -2,9 +2,9 @@ from .album import (
     AlbumView,
     AlbumListView,
     AlbumCategoryView,
-    AlbumTagView,
-    AlbumFavoriteView
+    AlbumTagView
 )
+from social.views import AlbumFavoriteView
 from permissions.views import AlbumPermissionView
 from .photo import (
     PhotoView,
