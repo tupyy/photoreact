4ddf25845bb5c66708fb13f134266b235b6f435b
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-28 09:58:33 +0200

    remove followed album model
    followed album is replaced by favorite album

--- back/src/social/models.py
+++ back/src/social/models.py
@@ -22,15 +22,3 @@ class FollowUser(models.Model):
                                         self.followed_user.username)
 
 
-class FollowAlbum(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = "followed album"
-        verbose_name_plural = "followed albums"
-
-    def __str__(self):
-        return "{}_follows_album_{}".format(self.user.username,
-                                            self.album.id)
-
