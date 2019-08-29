c6124ed74f7b1996b1b15bd9f6bcbf5b11ac99d0
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-08-29 17:36:14 +0200

    add update method

--- back/src/accounts/serializers.py
+++ back/src/accounts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from accounts.models import UserProfile
@@ -21,3 +23,15 @@ class UserProfileSerializer(serializers.ModelSerializer):
     def get_photo(self, obj):
         from photogallery.settings import STATIC_PROFILE_PHOTO_URL
         return s3_manager.get_get_signed_url(STATIC_PROFILE_PHOTO_URL + obj.photo_filename)
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+        first_name = user_data.pop('first_name')
+        last_name = user_data.pop('last_name')
+        email = user_data.pop('email')
+        user = get_object_or_404(User, pk=instance.id)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+        return instance
