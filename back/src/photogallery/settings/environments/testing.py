fe39c27402e6e020c45285e19429b348478b3d5f
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-08-29 20:01:17 +0200

    doc

--- back/src/photogallery/settings/environments/testing.py
+++ back/src/photogallery/settings/environments/testing.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 """
-This file contains all the settings that defines the development server.
-
-
 SECURITY WARNING: don't run with debug turned on in production!
 """
 import os
