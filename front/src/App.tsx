0abc03bcbb357d0ac936507560379d8669f22af2
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-07 08:47:15 +0200

    front stuff..still experimenting

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {hot} from 'react-hot-loader';
 
-export default App;
+import {IRootState} from 'app/shared/reducers';
+import {getSession} from 'app/shared/reducers/authentication';
+import {getProfile} from 'app/shared/reducers/application-profile';
+import {setLocale} from 'app/shared/reducers/locale';
+import {hasAnyAuthority} from 'app/shared/auth/private-route';
+import ErrorBoundary from 'app/shared/error/error-boundary';
+import {AUTHORITIES} from 'app/config/constants';
+import AppRoutes from 'app/routes';
+
+const baseHref = document
+    .querySelector('base')
+    .getAttribute('href')
+    .replace(/\/$/, '');
+
+export interface IAppProps extends StateProps, DispatchProps {}
+
+export const App = (props: IAppProps) => {
+    useEffect(() => {
+        props.getSession();
+    }, []);
+
+    const paddingTop = '60px';
+    return (
+        <Router basename={baseHref}>
+            <div className="app-container" style={{ paddingTop }}>
+                <div className="container-fluid view-container" id="app-view-container">
+                    <ErrorBoundary>
+                        <AppRoutes />
+                    </ErrorBoundary>
+                </div>
+            </div>
+        </Router>
+    );
+};
+
+const mapStateToProps = ({ authentication, applicationProfile, locale }: IRootState) => ({
+    currentLocale: locale.currentLocale,
+    isAuthenticated: authentication.isAuthenticated,
+    isAdmin: hasAnyAuthority(authentication.account.authorities, [AUTHORITIES.ADMIN]),
+    ribbonEnv: applicationProfile.ribbonEnv,
+    isInProduction: applicationProfile.inProduction,
+    isSwaggerEnabled: applicationProfile.isSwaggerEnabled
+});
+
+const mapDispatchToProps = { setLocale, getSession, getProfile };
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(hot(module)(App));
+
---
+++ front/src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Switch} from 'react-router-dom';
+
+import Login from 'app/modules/login/login';
+import Logout from 'app/modules/login/logout';
+import Home from 'app/modules/home/home';
+import PrivateRoute from 'app/shared/auth/private-route';
+import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
+import PageNotFound from 'app/shared/error/page-not-found';
+import {AUTHORITIES} from 'app/config/constants';
+
+
+const Routes = () => (
+  <div className="view-routes">
+    <Switch>
+      <ErrorBoundaryRoute path="/login" component={Login} />
+      <ErrorBoundaryRoute path="/logout" component={Logout} />
+      <PrivateRoute path="/" exact component={Home} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}/>
+      <ErrorBoundaryRoute component={PageNotFound} />
+    </Switch>
+  </div>
+);
+
+export default Routes;
