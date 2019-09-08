5d11b0476932193bb21887ef6cdaa467d236a4c2
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-08 20:09:35 +0200

    add media reducers

---
+++ front/src/shared/model/album.model.ts
@@ -0,0 +1,21 @@
+import { string } from "prop-types"
+
+export interface IAlbum {
+    id: number,
+    name?: string,
+    description?: string,
+    date: Date,
+    preview: string,
+    categories?: string[],
+    tags?: string[]
+}
+
+export const defaultValue: Readonly<IAlbum> = {
+    id: -1,
+    name: '',
+    description: '',
+    date: new Date(),
+    preview: '',
+    categories: [],
+    tags: [] 
+}
--- front/src/shared/reducers/index.ts
+++ front/src/shared/reducers/index.ts
@@ -3,18 +3,20 @@ import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar';
 
 import locale, { LocaleState } from './locale';
 import authentication, { AuthenticationState } from './authentication';
-
+import media, {MediaState} from './media';
 
 export interface IRootState {
   readonly authentication: AuthenticationState;
   readonly locale: LocaleState;
   readonly loadingBar: any;
+  readonly media: MediaState
 }
 
 const rootReducer = combineReducers<IRootState>({
   authentication,
   locale,
-  loadingBar
+  loadingBar,
+  media
 });
 
 export default rootReducer;
