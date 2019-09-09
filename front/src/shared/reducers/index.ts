7f6097621cac6bd3934e9c7cc5965af2ad6558cf
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-09 20:28:57 +0200

    add reducers for showing/hiding navbar

--- front/src/shared/reducers/index.ts
+++ front/src/shared/reducers/index.ts
@@ -3,20 +3,23 @@ import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar';
 
 import locale, { LocaleState } from './locale';
 import authentication, { AuthenticationState } from './authentication';
-import media, {AlbumState} from './album';
+import album, {AlbumState} from './album';
+import navbar, {NavBarState} from './navbar';
 
 export interface IRootState {
   readonly authentication: AuthenticationState;
   readonly locale: LocaleState;
   readonly loadingBar: any;
-  readonly media: AlbumState
+  readonly album: AlbumState
+  readonly navbar: NavBarState
 }
 
 const rootReducer = combineReducers<IRootState>({
   authentication,
   locale,
   loadingBar,
-  media
+  album,
+  navbar
 });
 
 export default rootReducer;
