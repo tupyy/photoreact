fd9f74cdd92c804adc0493e311121a8022ff6fc2
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-09 20:28:05 +0200

    hide navbar at login

--- front/src/modules/login/login.tsx
+++ front/src/modules/login/login.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import {Redirect, RouteComponentProps} from 'react-router-dom';
 
 import {IRootState} from 'app/shared/reducers';
 import {login} from 'app/shared/reducers/authentication';
+import {hideNavBar} from 'app/shared/reducers/navbar';
 import LoginForm from './login-form';
 
 export interface ILoginProps extends StateProps, DispatchProps, RouteComponentProps<{}> {}
 
 export const Login = (props: ILoginProps) => {
+    useEffect( () => {
+        props.hideNavBar();
+    }, [])
 
     const handleLogin = (username, password, rememberMe = false) => props.login(username, password, rememberMe);
 
@@ -20,12 +24,16 @@ export const Login = (props: ILoginProps) => {
     return <LoginForm handleLogin={handleLogin} loginError={props.loginError} />;
 };
 
-const mapStateToProps = ({ authentication }: IRootState) => ({
+const mapStateToProps = ({ authentication, navbar }: IRootState) => ({
     isAuthenticated: authentication.isAuthenticated,
     loginError: authentication.loginError,
+    navbar: navbar
 });
 
-const mapDispatchToProps = { login };
+const mapDispatchToProps = { 
+    login,
+    hideNavBar
+};
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
