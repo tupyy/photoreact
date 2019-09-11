5b12012a9f0ba8e02c3189601c3a01b4e4ee1e34
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-11 14:44:14 +0200

    move notification icon into a separate component

---
+++ front/src/shared/layout/navbar/navbar-notifications.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+const NavBarNotifications = () => {
+
+    return (
+        <IconButton aria-label="show 17 new notifications" color="inherit">
+            <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+            </Badge>
+        </IconButton>
+    )
+}
+
+export default NavBarNotifications;
