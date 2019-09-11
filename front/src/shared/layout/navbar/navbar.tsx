d3db3229e8afa476c4678d938fb6955307696c46
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-11 15:22:12 +0200

    move user profile menu into a separated component

--- front/src/shared/layout/navbar/navbar.tsx
+++ front/src/shared/layout/navbar/navbar.tsx
@@ -1,89 +1,17 @@
-import { AppBar, Badge, IconButton, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import NavBarNotifications from './navbar-notifications';
-import UserAvatar from 'app/shared/components/user_avatar/user-avatar';
 import { IRootState } from 'app/shared/reducers';
 import React from 'react';
 import { connect } from 'react-redux';
+import NavBarNotifications from './navbar-notifications';
 import useStyles from './navbar-styles';
+import UserMenu from './user-menu';
 
 export interface INavBarProps extends StateProps {};
 
 const NavBar = (props: INavBarProps) => {
     const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
-  
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  
-    function handleProfileMenuOpen(event: React.MouseEvent<HTMLElement>) {
-      setAnchorEl(event.currentTarget);
-    }
-  
-    function handleMobileMenuClose() {
-      setMobileMoreAnchorEl(null);
-    }
-  
-    function handleMenuClose() {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    }
-  
-    function handleMobileMenuOpen(event: React.MouseEvent<HTMLElement>) {
-      setMobileMoreAnchorEl(event.currentTarget);
-    }
-  
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={menuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
-        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-      </Menu>
-    );
-  
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        id={mobileMenuId}
-        keepMounted
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMobileMenuOpen}
-        onClose={handleMobileMenuClose}
-      >
-        <MenuItem>
-          <NavBarNotifications />
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={handleProfileMenuOpen}>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <UserAvatar 
-                firstName={props.userProfile.first_name}
-                lastName={props.userProfile.last_name}
-                profilePhoto={props.userProfile.photo}
-            />
-          </IconButton>
-          <p>Account</p>
-        </MenuItem>
-      </Menu>
-    );
   
     return (
       <div className={classes.grow}>
@@ -103,36 +31,13 @@ const NavBar = (props: INavBarProps) => {
             <div className={classes.grow} />
             <div className={classes.sectionDesktop}>
               <NavBarNotifications />
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-            <UserAvatar 
-                firstName={props.userProfile.first_name}
-                lastName={props.userProfile.last_name}
-                profilePhoto={props.userProfile.photo}
-            />
-            </IconButton>
+              <UserMenu userProfile={props.userProfile} sectionType="desktop" />
             </div>
             <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
+              <UserMenu userProfile={props.userProfile} sectionType="mobile" />
             </div>
           </Toolbar>
         </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
       </div>
     );
   }
