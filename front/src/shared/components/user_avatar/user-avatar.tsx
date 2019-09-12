933f698333cfd706178077877099589dad024bc8
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-12 16:14:42 +0200

    format 4 spaces

--- front/src/shared/components/user_avatar/user-avatar.tsx
+++ front/src/shared/components/user_avatar/user-avatar.tsx
@@ -1,52 +1,56 @@
 import React, {useState} from 'react';
-import Photo from 'app/shared/components/photo/photo';
-import { Avatar } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/core';
-import { red } from '@material-ui/core/colors';
+import {Avatar, makeStyles} from '@material-ui/core';
 import {getNameInitials} from "app/shared/util/user-name-utils";
 
 interface IUserAvatar {
-  firstName: string;
-  lastName: string;
-  profilePhoto?: string
-  size?: string
+    firstName: string;
+    lastName: string;
+    profilePhoto?: string
+    size?: string
 }
 
 const useStyles = makeStyles({
-  photo: {
-    width: '100%'
-  },
-  bigAvatar: {
-    margin: 10,
-    width: 120,
-    height: 120,
-  },
+    photo: {
+        width: '100%'
+    },
+    bigAvatar: {
+        display: 'block',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        margin: 10,
+        width: 90,
+        height: 90,
+    },
 });
 
 const UserAvatar = (props: IUserAvatar) => {
-  const [imgError, setImgError] = useState(false);
+    const [imgError, setImgError] = useState(false);
 
-  // @ts-ignore
-  const classes = useStyles();
+    // @ts-ignore
+    const classes = useStyles();
 
-  if (props.profilePhoto && !imgError) {
-    return (
-      <Avatar
-        className={props.size === 'xl' ? classes.bigAvatar : null}
-        src={props.profilePhoto}
-        imgProps={
-          { "onError": () => {setImgError(true)} }
-        }
-      >
-      </Avatar>
-    )
-  } else {
-    return (
-      <Avatar>
-        {getNameInitials(props.firstName, props.lastName)}
-      </Avatar>
-    )
-  }
+    if (props.profilePhoto && !imgError) {
+        return (
+            <Avatar
+                className={props.size === 'xl' ? classes.bigAvatar : null}
+                src={props.profilePhoto}
+                imgProps={
+                    {
+                        "onError": () => {
+                            setImgError(true)
+                        }
+                    }
+                }
+            >
+            </Avatar>
+        )
+    } else {
+        return (
+            <Avatar>
+                {getNameInitials(props.firstName, props.lastName)}
+            </Avatar>
+        )
+    }
 }
 
 export default UserAvatar;
