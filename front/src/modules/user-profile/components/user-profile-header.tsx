b485afc1969c7217146383fe88080a65bad535f7
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-12 16:13:40 +0200

    add style to user profile header

--- front/src/modules/user-profile/components/user-profile-header.tsx
+++ front/src/modules/user-profile/components/user-profile-header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography} from "@material-ui/core";
+import {Container, makeStyles, Typography} from "@material-ui/core";
 
 interface IUserProfileHeader {
     // component to show user avatar
@@ -7,16 +7,38 @@ interface IUserProfileHeader {
     title: string
 }
 
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginLeft: 0,
+        marginRight: 0,
+        padding: 0,
+        background: '#fafafa'
+
+    },
+    secondary: {
+        display: 'block'
+    },
+    title: {
+        fontSize: '23px'
+    }
+});
+
 /**
  * this component shows the header of the user profile page.
  */
 const UserProfileHeader = (props: IUserProfileHeader) => {
-  return (
-      <div>
-          {props.avatarComponent}
-          <Typography variant="h5">{props.title}</Typography>
-      </div>
-  )
+
+    const classes = useStyles();
+    return (
+        <Container className={classes.root}>
+            <div className={class.secondary}>
+                {props.avatarComponent}
+                <Typography variant="h6" className={classes.title}>{props.title}</Typography>
+            </div>
+        </Container>
+    )
 };
 
 export default UserProfileHeader;
