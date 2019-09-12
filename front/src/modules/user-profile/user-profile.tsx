ae088f91786317ac56faae9a1f05e298af3234ec
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-12 15:36:32 +0200

    insert user profile header

--- front/src/modules/user-profile/user-profile.tsx
+++ front/src/modules/user-profile/user-profile.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {IRootState} from "app/shared/reducers";
+import UserAvatar from "app/shared/components/user_avatar/user-avatar";
+import UserProfileHeader from "app/modules/user-profile/components/user-profile-header";
+import {getFullName} from "app/shared/util/user-name-utils";
 
 interface IUserProfileProps extends StateProps {
 
@@ -10,13 +13,30 @@ interface IUserProfileProps extends StateProps {
  * Main component to show the user profile.
  */
 const UserProfile = (props: IUserProfileProps) => {
-  return (
-      <div>UserProfile</div>
-  )
+
+    const getUserAvatarComponent = (userProfile) => {
+        return (
+            <UserAvatar
+                firstName={userProfile.first_name}
+                lastName={userProfile.last_name}
+                profilePhoto={userProfile.photo}
+                size="xl"
+            />
+        )
+    };
+
+    return (
+        <div>
+            <UserProfileHeader
+                avatarComponent={getUserAvatarComponent(props.account)}
+                title={getFullName(props.account.first_name, props.account.last_name)}
+            />
+        </div>
+    )
 };
 
-const mapStateToProps = ( {authentication} : IRootState) => ({
-  account: authentication.account
+const mapStateToProps = ({authentication}: IRootState) => ({
+    account: authentication.account
 });
 
 type StateProps = ReturnType<typeof mapStateToProps>;
