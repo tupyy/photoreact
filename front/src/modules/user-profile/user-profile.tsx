4d00a21eaaab30da3448a45493353e4268c1b2e8
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-13 08:13:06 +0200

    move getAvatarcomponent outside the main function

--- front/src/modules/user-profile/user-profile.tsx
+++ front/src/modules/user-profile/user-profile.tsx
@@ -5,27 +5,34 @@ import UserAvatar from "app/shared/components/user_avatar/user-avatar";
 import UserProfileHeader from "app/modules/user-profile/components/user-profile-header";
 import {getFullName} from "app/shared/util/user-name-utils";
 import UserProfileTabs from "app/modules/user-profile/components/user-profile-tabs";
-import {Container} from "@material-ui/core";
 
 interface IUserProfileProps extends StateProps {
 
 }
 
 const TabNames = ["Overview", "Activity", "Albums", "Permissions"];
+
+/**
+ * Return an user avatar component
+ */
+const getUserAvatarComponent = (userProfile) => {
+    return (
+        <UserAvatar
+            firstName={userProfile.first_name}
+            lastName={userProfile.last_name}
+            profilePhoto={userProfile.photo}
+            size="xl"
+        />
+    )
+};
+
 /**
  * Main component to show the user profile.
  */
 const UserProfile = (props: IUserProfileProps) => {
 
-    const getUserAvatarComponent = (userProfile) => {
-        return (
-            <UserAvatar
-                firstName={userProfile.first_name}
-                lastName={userProfile.last_name}
-                profilePhoto={userProfile.photo}
-                size="xl"
-            />
-        )
+    const handleTabChange = (event, newValue) => {
+       console.log("Tab changed: " + String(newValue));
     };
 
     return (
@@ -36,8 +43,7 @@ const UserProfile = (props: IUserProfileProps) => {
             />
             <UserProfileTabs
                 tabNames={TabNames}
-                handleTabChange={(event, newValue) => {
-                }}
+                handleTabChange={handleTabChange}
             />
         </div>
     )
