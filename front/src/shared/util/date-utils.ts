1282c0b378f25fddcfdaa64e5116f5941250b2dd
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-14 09:08:00 +0200

    add timedelta function

--- front/src/shared/util/date-utils.ts
+++ front/src/shared/util/date-utils.ts
@@ -1,7 +1,11 @@
 import moment from 'moment';
 
-import { APP_LOCAL_DATETIME_FORMAT, APP_LOCAL_DATETIME_FORMAT_Z } from 'app/config/constants';
+import { APP_LOCAL_DATETIME_FORMAT, APP_LOCAL_DATETIME_FORMAT_Z, APP_LOCAL_DATETIME_FORMAT_2 } from 'app/config/constants';
 
 export const convertDateTimeFromServer = date => (date ? moment(date).format(APP_LOCAL_DATETIME_FORMAT) : null);
 
 export const convertDateTimeToServer = date => (date ? moment(date, APP_LOCAL_DATETIME_FORMAT_Z).toDate() : null);
+
+export const timedelta = (days: number):string {
+    return moment().add(days, 'days').format(APP_LOCAL_DATETIME_FORMAT_2);
+}
