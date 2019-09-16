43a836087e0fd0af407d21b8c5ac48f1dcbb05af
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-16 18:57:50 +0200

    move redux logic from component to tab

--- front/src/modules/user-profile/components/activities-tab.tsx
+++ front/src/modules/user-profile/components/activities-tab.tsx
@@ -1,33 +1,86 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {TabContext} from "app/modules/user-profile/tab-context";
 import {makeStyles} from "@material-ui/core";
 import {Theme} from "@material-ui/core/styles/createMuiTheme";
+import {IActivity} from 'app/shared/model/activity.model';
 import {ITabComponent} from "app/modules/user-profile/tab-component-interface";
+import LoadingComponent from 'app/shared/components/loading/loading-component';
 import combineStyles from "app/shared/util/combine-styles";
 import useTabComponentStyles from "app/modules/user-profile/components/tab-component-styles";
 import ActivityComponent from 'app/modules/user-profile/components/activity-component';
+import {IRootState} from 'app/shared/reducers';
+import {getActivities} from 'app/shared/reducers/user-profile';
+import {connect} from 'react-redux';
 
-interface IActivity extends ITabComponent{
-    index: number
-}
+interface IActivityProps extends ITabComponent, StateProps, DispatchProps {}
 
 const useStyles = combineStyles(useTabComponentStyles, makeStyles( (theme: Theme) => ({
 })));
 
-const Activity: React.SFC<IActivity> = (props: IActivity) => {
+const Activity: React.SFC<IActivityProps> = (props: IActivityProps) => {
     const tabContext = useContext(TabContext);
+
+	useEffect( () => {
+		// Load data only once when the tab becomes visible for the first time
+		if (isVisible() && !hasData(props.activities) && !props.loading ) {
+			props.getActivities();
+		}
+	});
+
+	/**
+	 * Return true if the component is visible
+	 */
+	const isVisible = () => {
+		return props.index === tabContext.currentTab; 
+	}
+
+	/**
+	 * Return true if data has been fetched from server
+	 */
+	const hasData = (arr: Array<IActivity>) => {
+		return arr.length !== 0;
+	};
 
     // @ts-ignore
     const classes = useStyles();
-    return (
-        <div className={props.index !== tabContext.currentTab ? classes.hidden : classes.root}>
-            <ActivityComponent 
-				title="Activities"
-				visible={props.index == tabContext.currentTab}
-			>
-			</ActivityComponent>
-        </div>
-    )
+
+	/**
+	 * Render loading component if component is visible and data is fetching
+	 */
+	if (isVisible() && props.loading) {
+		return (
+			<LoadingComponent />
+		)
+	}
+	else if (isVisible() && hasData(props.activities)) {
+		return (
+			<div>
+				<div className={classes.root}>
+					<ActivityComponent
+						title="Activities"
+						data={props.activities}
+					/>
+				</div>
+			</div>
+			)
+	}
+	return (<div></div>);
 };
 
-export default Activity;
+const mapStateToProps = ({userProfile}: IRootState) => ({
+	loading: userProfile.loading,
+	activities: userProfile.activities,
+	errorMessage: userProfile.errorMessage
+});
+
+const mapDispatchToProps = {
+	getActivities
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Activity);
--- front/src/modules/user-profile/components/activity-component.tsx
+++ front/src/modules/user-profile/components/activity-component.tsx
@@ -1,39 +1,20 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Container } from '@material-ui/core';
 import ActivityItem from './activity-item';
 import {IActivity} from 'app/shared/model/activity.model';
-import {IRootState} from 'app/shared/reducers';
-import {getActivities} from 'app/shared/reducers/user-profile';
-import {connect} from 'react-redux';
 
-interface IActivityComponent extends StateProps, DispatchProps {
+export interface IActivityComponent {
     title: string,
-	visible: boolean
+	data: IActivity[]
 }
 
 /**
  * It renders the activities fetched from server. It can be embedded into Overview or Activities tabs
  */
 const ActivityComponent = (props: IActivityComponent) => {
-
-	useEffect( () => {
-		// Load data only once when the tab becomes visible for the first time
-		if (props.visible && !hasData(props.activities) && !props.loading ) {
-			props.getActivities();
-		}
-	});
-
-	const hasData = (arr: Array<IActivity>) => {
-		return arr.length !== 0;
-	};
-
-	if (props.loading) {
-		return <div>Loading...</div>;
-	};
-
     return (
         <Container maxWidth="xl">
-            {props.activities.map( (item: IActivity, index: number ) => 
+            {props.data.map( (item: IActivity, index: number ) => 
                 <ActivityItem 
                     key={String(index)}
                     id={item.content_object.id}
@@ -47,20 +28,4 @@ const ActivityComponent = (props: IActivityComponent) => {
     )
 };
 
-const mapStateToProps = ({userProfile}: IRootState) => ({
-	loading: userProfile.loading,
-	activities: userProfile.activities,
-	errorMessage: userProfile.errorMessage
-});
-
-const mapDispatchToProps = {
-	getActivities
-};
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(ActivityComponent);
+export default ActivityComponent;
