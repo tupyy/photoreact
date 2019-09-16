01650ef64fb421437281c5126015c60a33d7c319
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-16 16:40:59 +0200

    use redux instead of internally get

--- front/src/modules/user-profile/components/activity-component.tsx
+++ front/src/modules/user-profile/components/activity-component.tsx
@@ -1,38 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useEffect} from 'react';
 import { Container } from '@material-ui/core';
 import ActivityItem from './activity-item';
 import {IActivity} from 'app/shared/model/activity.model';
+import {IRootState} from 'app/shared/reducers';
+import {getActivities} from 'app/shared/reducers/user-profile';
+import {connect} from 'react-redux';
 
-interface IActivityComponent {
-    url: string,
+interface IActivityComponent extends StateProps, DispatchProps {
     title: string,
+	visible: boolean
 }
 
 /**
  * It renders the activities fetched from server. It can be embedded into Overview or Activities tabs
  */
 const ActivityComponent = (props: IActivityComponent) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
 
-    useEffect( () => {
-        const fetchData = async (url: string) => {
-            try {
-                const response = await axios.get(url);
-                if (response) {
-                    setData(response.data.results);
-                }
-            } catch (error) {
-                setError(error);
-            }
-        }
-        fetchData(props.url);
-    }, []);
+	useEffect( () => {
+		// Load data only once when the tab becomes visible for the first time
+		if (props.visible && !hasData(props.activities) && !props.loading ) {
+			props.getActivities();
+		}
+	});
+
+	const hasData = (arr: Array<IActivity>) => {
+		return arr.length !== 0;
+	};
+
+	if (props.loading) {
+		return <div>Loading...</div>;
+	};
 
     return (
         <Container maxWidth="xl">
-            {data.map( (item: IActivity, index: number ) => 
+            {props.activities.map( (item: IActivity, index: number ) => 
                 <ActivityItem 
                     key={String(index)}
                     id={item.content_object.id}
@@ -46,4 +47,20 @@ const ActivityComponent = (props: IActivityComponent) => {
     )
 };
 
-export default ActivityComponent;
+const mapStateToProps = ({userProfile}: IRootState) => ({
+	loading: userProfile.loading,
+	activities: userProfile.activities,
+	errorMessage: userProfile.errorMessage
+});
+
+const mapDispatchToProps = {
+	getActivities
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ActivityComponent);
