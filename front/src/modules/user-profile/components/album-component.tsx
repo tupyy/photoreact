b9f9faae12bebc27eb671327fed4a3f088f6dae9
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-16 21:03:48 +0200

    start of album component

---
+++ front/src/modules/user-profile/components/album-component.tsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+interface IAlbumProps {
+	url: string
+}
+
+const AlbumComponent = (props: IAlbumProps) => {
+	const [data, setData] = useState(null);
+	useEffect( () => {
+		async function fetchData
+	}
+}
