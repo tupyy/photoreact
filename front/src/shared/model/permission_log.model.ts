d816b3e66b099c03b367f09addd831298c62e684
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-19 15:28:56 +0200

    change type for date temporary

--- front/src/shared/model/permission_log.model.ts
+++ front/src/shared/model/permission_log.model.ts
@@ -1,4 +1,5 @@
 import {IAlbum} from 'app/shared/model/album.model';
+import moment from 'moment';
 
 export interface IContentObjectLog {
 	id: number,
@@ -10,29 +11,29 @@ export interface IPermissionLog {
 	contentObject: IContentObjectLog,
 	userFrom: string,
 	album: IAlbum,
-	date: Date,
+	date: string,
 	permission: string,
-	action: string
+	operation: string
 }
 
 export const getPermissionLogMockingData = (): IPermissionLog[] => {
-	let arr: IPermissionLog[];
+	let arr: IPermissionLog[] = [];
 	arr.push({
 		contentObject: {
 			id: 1,
-			name: "user1",
+			name: "Toto titi",
 			type: "user"
 		},
 		userFrom: "admin",
 		album: {
 			id: 1,
 			name: "album1",
-			date: new Date('2019-01-01T00:00:00'),
+			date:"dd", 
 			preview: "link"
 		},
 		permission: "add_photos",
-		date: new Date('2019-01-01T:00.00:00'),
-		action: "add"
+		date: moment().format('DD/MM/YYYY'),
+		operation: "add"
 	});
 
 	arr.push({
@@ -45,12 +46,12 @@ export const getPermissionLogMockingData = (): IPermissionLog[] => {
 		album: {
 			id: 2,
 			name: "album2",
-			date: new Date('2019-01-01T00:00:00'),
+			date: new Date('2019-01-01T00:00:00Z'),
 			preview: "link"
 		},
 		permission: 'change_album',
-		date: new Date('2019-01-01T:00.00:00'),
-		action: "modifiy"
+		date: moment().format('DD/MM/YYYY'),
+		operation: "modify"
 	});
 
 	arr.push({
@@ -63,12 +64,12 @@ export const getPermissionLogMockingData = (): IPermissionLog[] => {
 		album: {
 			id: 1,
 			name: "album1",
-			date: new Date('2019-01-01T00:00:00'),
+			date: new Date('2019-01-01T00:00:00Z'),
 			preview: "link"
 		},
 		permission: 'add_photos',
-		date: new Date('2019-01-01T:00.00:00'),
-		action: "delete"
+		date: moment().format('DD/MM/YYYY'),
+		operation: "delete"
 	});
 	return arr;
 }
