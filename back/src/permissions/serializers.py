217ebc9cc02250c0f629353881757eaaca830645
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-20 12:14:29 +0200

    if content object is a user serialize the user profile if not set name of the group

--- back/src/permissions/serializers.py
+++ back/src/permissions/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from accounts.models import UserProfile
+from accounts.serializers import UserProfileSerializer
 from permissions.models import PermissionLog
 
 
@@ -10,23 +12,37 @@ class ObjectRelatedField(serializers.RelatedField):
     """
 
     def to_representation(self, value):
-        if isinstance(value, User) or isinstance(value, Group):
-            return self.get_object_data(value)
+        if isinstance(value, User):
+            return self.get_user_data(value)
+        elif isinstance(value, Group):
+            return self.get_group_data(value)
         raise Exception("Unexpected type of object")
 
-    def get_object_data(self, value):
+    def get_common_data(self, value):
         data = dict()
         data['id'] = value.id
+        data['type'] = 'user' if isinstance(value, User) else 'group'
+        return data
+
+    def get_group_data(self, value):
+        data = self.get_common_data(value)
         data['name'] = str(value)
-        data['type'] = 'user' if isinstance(value,User) else 'group'
+        return data
+
+    def get_user_data(self, value):
+        data = self.get_common_data(value)
+        user_profile = UserProfile.objects.filter(user__username__exact=value.username).first()
+        if user_profile is not None:
+            data['profile'] = UserProfileSerializer(user_profile).data
         return data
 
 
 class PermissionLogSerializer(serializers.ModelSerializer):
     content_object = ObjectRelatedField(read_only=True)
     user_from = serializers.CharField(source='user_from.username')
+    permission = serializers.StringRelatedField(source='permission.codename')
 
     class Meta:
         model = PermissionLog
-        fields = ['user_from', 'content_object','date', 'permission', 'operation']
-        read_only_fields = ['user_from', 'content_object','date', 'permission', 'operation']
+        fields = ['user_from', 'content_object', 'date', 'permission', 'operation']
+        read_only_fields = ['user_from', 'content_object', 'date', 'permission', 'operation']
