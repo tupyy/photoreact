7d964fa1c4a3d4e908ef7b79f445e44e42139791
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-20 13:08:04 +0200

    change model according to server model

--- front/src/shared/model/permission_log.model.ts
+++ front/src/shared/model/permission_log.model.ts
@@ -1,76 +1,19 @@
 import {IAlbum} from 'app/shared/model/album.model';
-import moment from 'moment';
+import {IUser} from 'app/shared/model/user.model';
 
 export interface IContentObjectLog {
 	id: number,
-	name: string,
+	name?: string,
+	profile?: IUser,
 	type: string
 }
 
 export interface IPermissionLog {
-	contentObject: IContentObjectLog,
+	content_object: IContentObjectLog,
 	userFrom: string,
 	album: IAlbum,
 	date: string,
 	permission: string,
 	operation: string
-}
-
-export const getPermissionLogMockingData = (): IPermissionLog[] => {
-	let arr: IPermissionLog[] = [];
-	arr.push({
-		contentObject: {
-			id: 1,
-			name: "Toto titi",
-			type: "user"
-		},
-		userFrom: "admin",
-		album: {
-			id: 1,
-			name: "album1",
-			date:"dd", 
-			preview: "link"
-		},
-		permission: "add_photos",
-		date: moment().format('DD/MM/YYYY'),
-		operation: "add"
-	});
-
-	arr.push({
-		contentObject: {
-			id: 2,
-			name: "user2",
-			type: "user"
-		},
-		userFrom: "admin",
-		album: {
-			id: 2,
-			name: "album2",
-			date: new Date('2019-01-01T00:00:00Z'),
-			preview: "link"
-		},
-		permission: 'change_album',
-		date: moment().format('DD/MM/YYYY'),
-		operation: "modify"
-	});
-
-	arr.push({
-		contentObject: {
-			id: 1,
-			name: "user1",
-			type: "user"
-		},
-		userFrom: "admin",
-		album: {
-			id: 1,
-			name: "album1",
-			date: new Date('2019-01-01T00:00:00Z'),
-			preview: "link"
-		},
-		permission: 'add_photos',
-		date: moment().format('DD/MM/YYYY'),
-		operation: "delete"
-	});
-	return arr;
 }
 
