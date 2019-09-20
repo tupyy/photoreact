90f02785954b8a2eeac9fc63ab5e5cd79ecdb990
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-20 12:36:22 +0200

    import user profile APIs

--- front/src/shared/reducers/user-profile.ts
+++ front/src/shared/reducers/user-profile.ts
@@ -4,9 +4,7 @@ import {IPermissionLog} from 'app/shared/model/permission_log.model';
 import {getPermissionLogMockingData} from 'app/shared/model/permission_log.model';
 import {IActivity} from 'app/shared/model/activity.model';
 import axios from 'axios';
-import {timedelta} from 'app/shared/util/date-utils';
-import {APP_LOCAL_DATETIME_FORMAT_2} from 'app/config/constants';
-import moment from 'moment';
+import {API_USER_PROFILE_ALBUMS, API_USER_PROFILE_ACTIVITIES, API_USER_PROFILE_PERMISSION_LOG} from 'app/config/constants';
 
 export const ACTION_TYPES = {
 	GET_USER_ALBUMS: 'userProfile/GET_USER_ALBUMS',
@@ -19,12 +17,9 @@ const initialState = {
 	albums: [] as IAlbum[],
 	activities: [] as IActivity[],
 	errorMessage: null as string,
-	permissionLog: [] as IPermissionLog[]
+	permissionLogs: [] as IPermissionLog[]
 }
 
-const URL_ACTIVITIES = "api/activity?activity_from=" +timedelta(-7) + '&activity_to=' + moment().format(APP_LOCAL_DATETIME_FORMAT_2) + "&ordering=date";
-const URL_ALBUMS = "api/albums?limit=5";
-const URL_PERMISSION_LOG = "api/permission/log?permission_from=" + timedelta(-7) + '&permission_to=' + moment().format(APP_LOCAL_DATETIME_FORMAT_2) + "&ordering=date";
 
 export type UserProfileState = Readonly<typeof initialState>;
 
@@ -61,7 +56,7 @@ export default (state: UserProfileState = initialState, action): UserProfileStat
 			return {
 				...state,
 				loading: false,
-				permissionLog: action.payload
+				permissionLogs: action.payload.data.logs
 		};
 		default:
 			return state
@@ -71,21 +66,21 @@ export default (state: UserProfileState = initialState, action): UserProfileStat
 export const getAlbums = () => async (dispatch) => {
 	await dispatch({
 		type: ACTION_TYPES.GET_USER_ALBUMS,
-		payload: axios.get(URL_ALBUMS)
+		payload: axios.get(API_USER_PROFILE_ALBUMS)
 	});
 };
 
 export const getActivities = () => async (dispatch) => {
 	await dispatch({
 		type: ACTION_TYPES.GET_USER_ACTIVITY,
-		payload: axios.get(URL_ACTIVITIES)
+		payload: axios.get(API_USER_PROFILE_ACTIVITIES)
 	});
 };
 
 export const getPermissionLogs = () => async (dispatch) => {
 	await dispatch({
 		type: ACTION_TYPES.GET_USER_PERMISSION_LOG,
-		payload: getPermissionLogData()
+		payload: axios.get(API_USER_PROFILE_PERMISSION_LOG)
 	});
 };
 
