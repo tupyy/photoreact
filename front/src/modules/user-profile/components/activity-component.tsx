6052463e55db0c8666bfdaee7bc89e934f6db5b8
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-20 13:45:53 +0200

    show nodata component if props.data is empty

--- front/src/modules/user-profile/components/activity-component.tsx
+++ front/src/modules/user-profile/components/activity-component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import ActivityItem from './activity-item';
 import {IActivity} from 'app/shared/model/activity.model';
+import NoData from 'app/shared/components/no-data/no-data';
 
 export interface IActivityComponent {
     title: string,
@@ -12,9 +13,10 @@ export interface IActivityComponent {
  * It renders the activities fetched from server. It can be embedded into Overview or Activities tabs
  */
 const ActivityComponent = (props: IActivityComponent) => {
-    return (
-        <Container maxWidth="xl">
-            {props.data.map( (item: IActivity, index: number ) => 
+
+	const renderItems = (arr: IActivity[]) => {
+		return (
+            arr.map( (item: IActivity, index: number ) => 
                 <ActivityItem 
                     key={String(index)}
                     id={item.content_object.id}
@@ -23,7 +25,12 @@ const ActivityComponent = (props: IActivityComponent) => {
                     activity={item.activity}
                     user={item.user}
                 />
-            )}
+		   ))
+	}
+
+    return (
+        <Container maxWidth="xl">
+			{props.data.length > 0 ? (renderItems(props.data)) : (<NoData />)}
         </Container>
     )
 };
--- front/src/modules/user-profile/components/permission-component.tsx
+++ front/src/modules/user-profile/components/permission-component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {IPermissionLog} from 'app/shared/model/permission_log.model';
 import {Container} from '@material-ui/core';
 import PermissionLogItem from 'app/modules/user-profile/components/permission-log-item';
+import NoData from 'app/shared/components/no-data/no-data';
 
 export interface IPermissionComponent {
 	title: string,
@@ -9,14 +10,19 @@ export interface IPermissionComponent {
 }
 
 const PermissionComponent = (props: IPermissionComponent) => {
-	return (
-		<Container maxWidth="xl">
-			{props.data.map( (item:IPermissionLog, index:number) => 
+	const renderItems = (arr: IPermissionLog[]) => {
+		return (
+			arr.map( (item:IPermissionLog, index:number) => 
 				<PermissionLogItem 
 					key={index.toString()}
 					permissionLog={item}
 				/>
-				)}
+			))
+	}
+
+	return (
+		<Container maxWidth="xl">
+			{props.data.length > 0 ? (renderItems(props.data)) : (<NoData />)}
 		</Container>
 	)
 };
