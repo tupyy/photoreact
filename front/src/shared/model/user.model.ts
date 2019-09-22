89754a825ead6247bb4c341fac6817b4e6b98cfc
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-22 16:42:56 +0200

    add missing member photo

--- front/src/shared/model/user.model.ts
+++ front/src/shared/model/user.model.ts
@@ -1,12 +1,13 @@
 export interface IUser {
-  id?: any;
-  active?: boolean,
-  username?: string;
-  first_name?: string;
-  last_name?: string;
-  email?: string;
-  roles?: any[];
-  langKey: string;
-  password?: string;
+	id?: any;
+	active?: boolean,
+	username?: string;
+	first_name?: string;
+	last_name?: string;
+	email?: string;
+	roles?: any[];
+	langKey: string;
+	password?: string;
+	photo?: string;
 };
 
