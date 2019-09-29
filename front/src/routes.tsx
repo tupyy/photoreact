4dedd497c95489aadfd0c453107173887fce17c3
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-29 08:35:10 +0200

    add route to album view

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -5,6 +5,7 @@ import Login from 'app/modules/login/login';
 import Logout from 'app/modules/login/logout';
 import Home from 'app/modules/home/home';
 import UserProfile from 'app/modules/user-profile/user-profile';
+import AlbumView from 'app/modules/album-view/album-view';
 import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import PageNotFound from 'app/shared/error/page-not-found';
@@ -18,6 +19,7 @@ const Routes = () => (
       <ErrorBoundaryRoute path="/logout" component={Logout} />
       <PrivateRoute path="/" exact component={Home} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}/>
       <PrivateRoute path="/profile" exact component={UserProfile} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}/>
+	  <PrivateRoute path="/album/:id" exact component={AlbumView} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]}/>
       <ErrorBoundaryRoute component={PageNotFound} />
     </Switch>
   </div>
