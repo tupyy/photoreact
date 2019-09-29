4b7b02453410d4ba4892e7b2156aa24985fb0346
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-29 08:34:07 +0200

    album reducer

--- front/src/shared/reducers/album.ts
+++ front/src/shared/reducers/album.ts
@@ -1,36 +1,45 @@
 import {FAILURE, REQUEST, SUCCESS} from 'app/shared/reducers/action-type.util';
+import {IPhoto} from 'app/shared/model/photo';
 import axios from 'axios';
 
 export const ACTION_TYPES = {
-    GET_RECENT_ALBUMS : 'media/GET_RECENT_ALBUMS'
+    GET_PHOTOS : 'album/GET_PHOTOS',
+	CHANGE_MODE: 'album/CHANGE_MODE'
+}
+
+export const MODE_TYPES = {
+	VIEW_MODE : 'viewMode',
+	EDIT_MODE: 'editMode'
 }
 
 const initialState = {
     loading: false,
     errorMessage: null as string,
     album: null,
-    recentAlbums: []
+	photos: null as IPhoto[],
+	mode: MODE_TYPES.VIEW_MODE,
+	selectedPhotos: null as IPhoto[]
 }
 
 export type AlbumState = Readonly<typeof initialState>;
 
 export default (state: AlbumState = initialState, action) : AlbumState => {
     switch(action.type) {
-        case REQUEST(ACTION_TYPES.GET_RECENT_ALBUMS):
+        case REQUEST(ACTION_TYPES.GET_PHOTOS):
             return {
                 ...state,
                 loading: true
             };
-        case FAILURE(ACTION_TYPES.GET_RECENT_ALBUMS):
+        case FAILURE(ACTION_TYPES.GET_PHOTOS):
             return {
                 ...initialState,
                 errorMessage: action.payload
             };
-        case SUCCESS(ACTION_TYPES.GET_RECENT_ALBUMS):
+        case SUCCESS(ACTION_TYPES.GET_PHOTOS):
             return {
                 ...state,
                 loading: false,
-                recentAlbums: action.payload.data.albums
+                photos: action.payload.data
             }
         default:
             return state;
@@ -38,9 +47,9 @@ export default (state: AlbumState = initialState, action) : AlbumState => {
 }    
 
 
-export const getRecentAlbums = () => async (dispatch) => {
+export const getPhotos = (id:string) => async (dispatch) => {
     await dispatch({
-        type: ACTION_TYPES.GET_RECENT_ALBUMS,
-        payload: axios.get('api/albums?limit=5')
+        type: ACTION_TYPES.GET_PHOTOS,
+        payload: axios.get('api/photos/' + id + '/')
     });
 }
