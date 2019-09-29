24ccbcfef228ed1651ee2e70250e424805c2567e
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-09-29 08:34:28 +0200

    add album reducer to root state

--- front/src/shared/reducers/index.ts
+++ front/src/shared/reducers/index.ts
@@ -3,26 +3,29 @@ import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar';
 
 import locale, { LocaleState } from './locale';
 import authentication, { AuthenticationState } from './authentication';
-import albums, {AlbumsState} from './albums';
-import navbar, {NavBarState} from './navbar';
-import userProfile, {UserProfileState} from './user-profile';
+import albums, { AlbumsState } from './albums';
+import album, { AlbumState } from './album';
+import navbar, { NavBarState } from './navbar';
+import userProfile, { UserProfileState } from './user-profile';
 
 export interface IRootState {
-  readonly authentication: AuthenticationState;
-  readonly locale: LocaleState;
-  readonly loadingBar: any;
-  readonly albums: AlbumsState;
-  readonly navbar: NavBarState;
-  readonly userProfile: UserProfileState;
+	readonly authentication: AuthenticationState;
+	readonly locale: LocaleState;
+	readonly loadingBar: any;
+	readonly albums: AlbumsState;
+	readonly navbar: NavBarState;
+	readonly userProfile: UserProfileState;
+	readonly album: AlbumState;
 }
 
 const rootReducer = combineReducers<IRootState>({
-  authentication,
-  locale,
-  loadingBar,
-  albums,
-  navbar,
-  userProfile
+	authentication,
+	locale,
+	loadingBar,
+	albums,
+	navbar,
+	userProfile,
+	album
 });
 
 export default rootReducer;
