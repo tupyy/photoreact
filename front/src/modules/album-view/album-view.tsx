1d7ada4b64bf2d096fa110ddd18d396aba12253b
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-09-30 14:21:28 +0200

    render album view

--- front/src/modules/album-view/album-view.tsx
+++ front/src/modules/album-view/album-view.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {IRootState} from 'app/shared/reducers';
 import {getPhotos} from 'app/shared/reducers/album';
+import PhotoContainer from 'app/modules/album-view/photo-container';
+import LoadingComponent from 'app/shared/components/loading/loading-component';
 
 interface IAlbumView extends StateProps, DispatchProps {
 	match: {}
 }
 
 export const AlbumView = (props: IAlbumView) => {
+	useEffect( () => {
+		props.getPhotos(props.match.params.id);
+	}, []);
+
+
+	const renderPhotos = () => {
+		if (props.photos) {
+			return (
+				<PhotoContainer 
+					photos={props.photos}
+					selectedPhotos={props.selectedPhotos}
+				/>);
+		} else {
+			return (<div>None</div>);
+		}
+	};
+
 	return (
-		<div>{props.match.params.id}</div>
+		<div>
+			{ props.loading ? (
+				<LoadingComponent />
+			): (
+				renderPhotos()
+			)}
+		</div>
 	)
 };
 
 const mapStateToProps = ({album}: IRootState) => ({
 	loading: album.loading,
 	errorMessage: album.errorMessage,
-	album: album.album,
+    album: album.album,
 	photos: album.photos,
 	mode: album.mode,
 	selectedPhotos: album.selectedPhotos
