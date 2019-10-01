c070ece70df30a6ef62728aef2d702c6541f1f63
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-10-01 09:43:36 +0200

    add a new prop selected.
    remove className prop because was useless

--- front/src/shared/components/photo/photo.tsx
+++ front/src/shared/components/photo/photo.tsx
@@ -1,27 +1,31 @@
-import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core';
 import { CircularProgress } from '@material-ui/core';
 
 export interface IPhotoProps {
     url: string;
     name?: string;
-    className: any;
+	selected: boolean;
 }
 
 const useStyles = makeStyles(theme => ({
     progress: {
         margin: theme.spacing(2),
       },
+	photo: {},
+	selected: {
+	}
 }));
 
 export default function Photo(props: IPhotoProps) {
     const [loading, setLoading] = useState(true);
 
     const classes = useStyles();
+	const selected = props.selected ? classes.selected : '';
+	const imgClass = `${classes.photo} ${selected}`;
     return (
         <div>
-            <img className={props.className} src={props.url} onLoad={() => setLoading(false)} alt={props.name} />
+            <img className={imgClass} src={props.url} onLoad={() => setLoading(false)} alt={props.name} />
             {loading && 
                 <CircularProgress className={classes.progress} />
             }
