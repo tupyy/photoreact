e550bcebda4083274d2d9ff4c691775637248af9
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-10-01 12:33:59 +0200

    render selected photo with check circle icon
    add some css

--- front/src/shared/components/photo/photo.tsx
+++ front/src/shared/components/photo/photo.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core';
 import { CircularProgress } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons/faCheckCircle';
 
 export interface IPhotoProps {
     url: string;
@@ -12,8 +14,21 @@ const useStyles = makeStyles(theme => ({
     progress: {
         margin: theme.spacing(2),
       },
-	photo: {},
-	selected: {
+	  photo: {
+		  position: 'relative'
+	},
+	selectedPhoto: {
+		position: 'relative',
+		opacity: 0.5
+	},
+	selectionIcon: {
+		position: 'absolute',
+		top: '30%',
+		left: '50%',
+		zIndex: 100,
+		width: 100,
+		height: 100,
+		color: 'green',
 	}
 }));
 
@@ -21,11 +36,22 @@ export default function Photo(props: IPhotoProps) {
     const [loading, setLoading] = useState(true);
 
     const classes = useStyles();
-	const selected = props.selected ? classes.selected : '';
-	const imgClass = `${classes.photo} ${selected}`;
-    return (
+	const selected = props.selected ? classes.selectionIcon : '';
+
+	const renderSelectedPhoto = () => {
+		return (
+			<div>
+				<FontAwesomeIcon icon={faCheckCircle} className={classes.selectionIcon} size="lg" />
+				<img className={classes.selectedPhoto} src={props.url} onLoad={() => setLoading(false)} alt={props.name} />
+			</div>
+		);
+	}
+
+	return (
         <div>
-            <img className={imgClass} src={props.url} onLoad={() => setLoading(false)} alt={props.name} />
+			{selected ? ( renderSelectedPhoto() ) : (
+	            <img className={classes.photo} src={props.url} onLoad={() => setLoading(false)} alt={props.name} />
+			)}
             {loading && 
                 <CircularProgress className={classes.progress} />
             }
